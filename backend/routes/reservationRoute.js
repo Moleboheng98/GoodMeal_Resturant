2af90f7e4a3390332ed59dfc87c5ee08eaa2fa1b
Moleboheng Mofokeng <molebohengmofokeng30@gmail.com>
2025-12-12 23:27:23 +0200

    fixed route

--- backend/routes/reservationRoute.js
+++ backend/routes/reservationRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import {createReservation,getALLReservation,deleteReservation} from '../controllers/reservationCotrollers.js'
+import { createReservation, getALLReservation, deleteReservation } 
+from '../controllers/reservationCotrollers.js'
 
 const Router = express.Router()
 
-Routerouter.post('/create',createReservation)
-Router.get('/get',getALLReservation)
-Router.delete('/delete/:id',deleteReservation)
+Router.post('/create', createReservation)
+Router.get('/get', getALLReservation)
+Router.delete('/delete/:id', deleteReservation)
 
 export default Router
